c4207a88c3b9b3fa9a0b485d8f421e8dc0a97724
zianwangs <Wzan980311@163.com>
2020-03-05 15:34:24 +0800

    completed the first part of lab1

---
+++ lab1/HelloNumbers.java
@@ -0,0 +1,11 @@
+public class HelloNumbers{
+	public static void main(String[] args) {
+		int t = 0, sum = 0;
+
+		while (t < 10){
+			System.out.prinlt(sum + " ");
+			t++;
+			sum = sum + t;
+		}
+	}
+}
